38130485d52ad08ce832e2a06aa363b595abd734
Maladrumthrasky Louh <Maladrumthrasky Louh>
2017-03-15 22:03:25 +0800

    Structure completed.

--- pages.js
+++ pages.js
@@ -1,8 +1,25 @@
+const MongoDB=require("mongodb").MongoClient;
+const ObjectId=require("mongodb").ObjectID;
+
+
+
+
 function Pages()
 {
     this.init=function(app,callback)
     {
-        process.nextTick(callback);
+        console.log("Connect to database...");
+        MongoDB.connect("mongodb://127.0.0.1:27017/easyChatPlatform",function(err,db)
+        {
+            if (err)
+            {
+                console.log(err.message);
+                process.exit(0);
+            }
+            database=db;
+            console.log("Database connected");
+            process.nextTick(callback);
+        });
     };
 }
 
--- webIfaces.js
+++ webIfaces.js
@@ -1,8 +1,27 @@
+const MongoDB=require("mongodb").MongoClient;
+const ObjectId=require("mongodb").ObjectID;
+
+
+
+
+
 function WebIFaces()
 {
+    let database;
     this.init=function(app,callback)
     {
-        process.nextTick(callback);
+        console.log("Connect to database...");
+        MongoDB.connect("mongodb://127.0.0.1:27017/easyChatPlatform",function(err,db)
+        {
+            if (err)
+            {
+                console.log(err.message);
+                process.exit(0);
+            }
+            database=db;
+            console.log("Database connected");
+            process.nextTick(callback);
+        });
     };
 }
 
