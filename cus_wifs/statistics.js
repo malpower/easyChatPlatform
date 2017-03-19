668d4d4db1374fe80342b364142065fa2c0fb3a9
Seven <George_zhang@jabil.com>
2017-03-19 22:26:49 +0800

    add Statistics for getProvincePassRateReport

--- cus_wifs/statistics.js
+++ cus_wifs/statistics.js
@@ -31,6 +31,35 @@ function Statistics()
                 return res.end(JSON.stringify(o));
             });
         });
+
+        app.bind("/getProvincePassReportByProvince",function(req,res){
+            let json=format.getReqJson(req);
+            if(!json){
+                return res.end(JSON.stringify({error:true,code:1,message:"Invalid JSON structure."}));
+            }
+            db.collection("Samples").find({createTime: {$gte: json.startTime,$lt: json.endTime}}).toArray(function(err,list){
+                if(err){
+                    return res.end(JSON.stringify({error:true,code:2,message:err.message}));
+                }
+                let calc=new Object;
+                for(let i=0;i<list.length;i++){
+                    if(calc[list[i].checkState]===undefined) continue;
+                    calc[list[i].userInfo.proAddress].allcount++;
+                    if(calc[list[i].userInfo]===undefined)  continue;
+                    if(calc[list[i].userInfo.proAddress]===undefined)  continue;
+                    if(calc[list[i].checkState]=="4"){
+                        calc[list[i].userInfo.proAddress].publishcount++;
+                    }
+                }
+                for(let i in calc){
+                      calc[i].passrate=function(){
+                          return (Math.round(calc[i].publishcount / calc[i].allcount * 10000) / 100.00 + "%");
+                      };
+                }
+                let o={error: false,statistics: calc,total: list.length};
+                return res.end(JSON.stringify(o));
+            });
+        });
     };
 }
 
