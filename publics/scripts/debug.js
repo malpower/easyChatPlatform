da8256eb713c3f8c5a1453831f9c6d66f1729efe
Maladrumthrasky <malpower@ymail.com>
2017-03-20 15:22:10 +0800

    SDLFJ

--- publics/scripts/debug.js
+++ publics/scripts/debug.js
@@ -2,13 +2,22 @@ $(document).ready(function()
 {
     let data={category: "Samples",content: {title: "SDLKFJ"}};
     let api=new WebIF;
-    $.ajax({url: "http://localhost/wif/data/create",
-            type: "POST",
+    $.ajax({url: "http://localhost/user/sign",
+            type: "GET",
             dataType: "JSON",
-            data: JSON.stringify(data),
             xhrFields:{withCredentials: true},
             success: function(res)
             {
                 console.log(res);
+                $.ajax({url: "http://localhost/wif/data/create",
+                        type: "POST",
+                        dataType: "JSON",
+                        data: JSON.stringify(data),
+                        xhrFields:{withCredentials: true},
+                        success: function(res)
+                        {
+                            console.log(res);
+                        }});
             }});
+
 });
--- utils/limiter.js
+++ utils/limiter.js
@@ -58,6 +58,14 @@ lim.addLimiter("Statistics.create",function(json,req,callback)
         {
             return callback(err);
         }
+        if (json.content.type==="perfect")
+        {
+            if (list[0].isPerfect===true)
+            {
+                return callback(new Error("Can't set the perfect field when a sample is perfect."));
+            }
+            database.collection("Samples").update({_id: list[0]._id},${set: {isPerfect: true}});
+        }
         json.content.case=list[0];
         callback(undefined,json);
     });
