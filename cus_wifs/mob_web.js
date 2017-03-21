b94d4dda58c21a2dcd5f00aa0c87913393454913
Maladrumthrasky Louh <malpower@ymail.com>
2017-03-21 19:30:20 +0800

    bug fixed.

--- cus_wifs/mob_web.js
+++ cus_wifs/mob_web.js
@@ -33,7 +33,34 @@ function MobWeb()
                     {
                         return res.end(JSON.stringify({error: true,code: 2,message: "Invalid openId"}));
                     }
-                    db.collection("Samples").update({_id: new ObjectId(caseId)},{addToSet: {"liked": {user: list[0]._id.toString(),createTime: (new Date).getTime()}}});
+                    db.collection("Samples").update({_id: new ObjectId(caseId)},{$addToSet: {"liked": {user: list[0]._id.toString(),createTime: (new Date).getTime()}}});
+                    res.end(JSON.stringify({error: false}));
+                });
+            });
+            app.post("/unlike",function(req,res)
+            {
+                let json=format.getReqJson(req);
+                if (!json)
+                {
+                    return res.end(JSON.stringify({error: true,code: 1,message: "Invalid request json format."}));
+                }
+                let openId=json.openId;
+                let caseId=json.caseId;
+                if (!openId && !caseId)
+                {
+                    return res.end(JSON.stringify({error: true,code: 3,message: "caseId and openId are required"}));
+                }
+                db.collection("Users").find({openId: openId}).toArray(function(err,list)
+                {
+                    if (err)
+                    {
+                        return res.end(JSON.stringify({error: true,code: 2,message: err.message}));
+                    }
+                    if (list.length===0)
+                    {
+                        return res.end(JSON.stringify({error: true,code: 2,message: "Invalid openId"}));
+                    }
+                    db.collection("Samples").update({_id: new ObjectId(caseId)},{$pull: {"liked": {user: list[0]._id.toString()}}});
                     res.end(JSON.stringify({error: false}));
                 });
             });
@@ -60,7 +87,7 @@ function MobWeb()
                     {
                         return res.end(JSON.stringify({error: true,code: 2,message: "Invalid openId"}));
                     }
-                    db.collection("Samples").update({_id: new ObjectId(caseId)},{addToSet: {"visited": {user: list[0]._id.toString(),createTime: (new Date).getTime()}}});
+                    db.collection("Samples").update({_id: new ObjectId(caseId)},{$addToSet: {"visited": {user: list[0]._id.toString(),createTime: (new Date).getTime()}}});
                     res.end(JSON.stringify({error: false}));
                 });
             });
