f5108da0b3aac805642e1490a98b2b261f549a7a
Maladrumthrasky <malpower@ymail.com>
2017-03-22 13:43:27 +0800

    addresses updated.

--- configurations/mob_addresses.js
+++ configurations/mob_addresses.js
@@ -1,14 +1,14 @@
-let addr={"a1": "/mob/fe-yixin/views/indestrysel.html?",                //大事件
-          "a2": "/mob/fe-yixin/views/industrysel.html?",                //行业精选
-          "a3": "/mob/fe-yixin/views/planningLayout.html?p=bujuguihua&",                //策划布局
-          "a4": "/mob/fe-yixin/views/planningLayout.html?p=mendiantuozhan&",                //门店拓展
-          "a5": "/mob/fe-yixin/views/planningLayout.html?p=xiaoshoucehua&",                    //销售策划
-          "a6": "/mob/fe-yixin/views/planningLayout.html?p=duiwujianshe&",                //队伍建设
-          "a7": "/mob/fe-yixin/views/planningLayout.html?p=shujuxitong&",                //数据系统
-          "a8": "/mob/fe-yixin/views/Mypoints.html?",                //我的积分
-          "a9": "/mob/fe-yixin/views/myshare.html?",                //我的分享
-          "a11": "/mob/fe-yixin/views/planningLayout.html?p=xiaonengtisheng&",                //我的分享
-          "a10": "/mob/fe-yixin/views/mycollect.html?"};              //我的收藏
+let addr={"a1": "/mob/views/indestrysel.html?",                //大事件
+          "a2": "/mob/views/industrysel.html?",                //行业精选
+          "a3": "/mob/views/planningLayout.html?p=bujuguihua&",                //策划布局
+          "a4": "/mob/views/planningLayout.html?p=mendiantuozhan&",                //门店拓展
+          "a5": "/mob/views/planningLayout.html?p=xiaoshoucehua&",                    //销售策划
+          "a6": "/mob/views/planningLayout.html?p=duiwujianshe&",                //队伍建设
+          "a7": "/mob/views/planningLayout.html?p=shujuxitong&",                //数据系统
+          "a8": "/mob/views/Mypoints.html?",                //我的积分
+          "a9": "/mob/views/myshare.html?",                //我的分享
+          "a11": "/mob/views/planningLayout.html?p=xiaonengtisheng&",                //我的分享
+          "a10": "/mob/views/mycollect.html?"};              //我的收藏
 
 
 module.exports=addr;
