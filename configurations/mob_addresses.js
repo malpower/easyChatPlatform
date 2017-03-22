a9b86d32bbfe2379ed47611c10341b81ec1132b5
Maladrumthrasky <malpower@ymail.com>
2017-03-22 13:38:47 +0800

    address updated.

--- configurations/mob_addresses.js
+++ configurations/mob_addresses.js
@@ -1,14 +1,14 @@
-let addr={"a1": "/fe-yixin/views/indestrysel.html?",                //大事件
-          "a2": "/fe-yixin/views/industrysel.html?",                //行业精选
-          "a3": "/fe-yixin/views/planningLayout.html?p=bujuguihua&",                //策划布局
-          "a4": "/fe-yixin/views/planningLayout.html?p=mendiantuozhan&",                //门店拓展
-          "a5": "/fe-yixin/views/planningLayout.html?p=xiaoshoucehua&",                    //销售策划
-          "a6": "/fe-yixin/views/planningLayout.html?p=duiwujianshe&",                //队伍建设
-          "a7": "/fe-yixin/views/planningLayout.html?p=shujuxitong&",                //数据系统
-          "a8": "/fe-yixin/views/Mypoints.html?",                //我的积分
-          "a9": "/fe-yixin/views/myshare.html?",                //我的分享
-          "a11": "/fe-yixin/views/planningLayout.html?p=xiaonengtisheng&",                //我的分享
-          "a10": "/fe-yixin/views/mycollect.html?"};              //我的收藏
+let addr={"a1": "/mob/fe-yixin/views/indestrysel.html?",                //大事件
+          "a2": "/mob/fe-yixin/views/industrysel.html?",                //行业精选
+          "a3": "/mob/fe-yixin/views/planningLayout.html?p=bujuguihua&",                //策划布局
+          "a4": "/mob/fe-yixin/views/planningLayout.html?p=mendiantuozhan&",                //门店拓展
+          "a5": "/mob/fe-yixin/views/planningLayout.html?p=xiaoshoucehua&",                    //销售策划
+          "a6": "/mob/fe-yixin/views/planningLayout.html?p=duiwujianshe&",                //队伍建设
+          "a7": "/mob/fe-yixin/views/planningLayout.html?p=shujuxitong&",                //数据系统
+          "a8": "/mob/fe-yixin/views/Mypoints.html?",                //我的积分
+          "a9": "/mob/fe-yixin/views/myshare.html?",                //我的分享
+          "a11": "/mob/fe-yixin/views/planningLayout.html?p=xiaonengtisheng&",                //我的分享
+          "a10": "/mob/fe-yixin/views/mycollect.html?"};              //我的收藏
 
 
 module.exports=addr;
