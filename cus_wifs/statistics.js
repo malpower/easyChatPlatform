9061fee6b11da4ac6f593bbc12ad0b028886c8ce
Maladrumthrasky <malpower@ymail.com>
2017-03-24 18:18:04 +0800

    top 10 bug fixed.

--- cus_wifs/statistics.js
+++ cus_wifs/statistics.js
@@ -69,6 +69,7 @@ function Statistics()
                         }
                         return 1;
                     });
+                    list.length=(list.length>10?10:list.length);
                     res.end(JSON.stringify({error: false,list: list}));
                 });
             });
@@ -103,6 +104,7 @@ function Statistics()
                         }
                         return 1;
                     });
+                    list.length=(list.length>10?10:list.length);
                     res.end(JSON.stringify({error: false,list: list}));
                 });
             });
