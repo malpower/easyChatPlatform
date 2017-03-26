3a0dfdf0d9a8d750ad11fc3b8aae7574a27f4a1e
Thrasky <Thrasky_Lou@Jabil.com>
2017-03-26 21:03:50 +0800

    invitition address changed.

--- configurations/easyChat.js
+++ configurations/easyChat.js
@@ -1,4 +1,4 @@
 module.exports={appId: "e1c52c6f31364914a520f2b636308e2f",              //easy chat APP ID
                 appSec: "eeff237ba65e40baa1cef96c473ffd69",             //easy chat APP SEC
-                invitationText: "请登录系统@  http://dqzy.internal-i-focusing.com/",         //invitation text for the subscribed users.
+                invitationText: "请登录系统@  http://qdzy.internal-i-focusing.com/",         //invitation text for the subscribed users.
                 refreshFreq: 1000*60*30};                               //easy chat access token refresh frequence. better in one hour.
