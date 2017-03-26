424927c53715b19cc3935b189e71ab473cde8d83
Thrasky <Thrasky_Lou@Jabil.com>
2017-03-26 21:26:23 +0800

    WTF

--- utils/csv_exporter.js
+++ utils/csv_exporter.js
@@ -5,7 +5,7 @@ function respond(list,title,header,res)
         header=header.split(",");
     }
     res.set("Content-Type","application/xls");
-    res.set("Content-DIsposition","attachment;filename=Export.xls");
+    res.set("Content-Disposition","attachment;filename=Export.xls");
     let content=`<tr><td cols="${header.length}">${title}</td><tr>`;
     content+=`<tr><td>${header.join("</td><td>")}</td></tr>`;
     for (let i=0;i<list.length;i++)
@@ -13,7 +13,7 @@ function respond(list,title,header,res)
         list[i]="<td>"+list[i].join("</td><td>")+"</td>";
     }
     content+="<tr>"+list.join("</tr><tr>")+"</tr>"
-    res.end(`<meta charset="utf-8" /><table><tbody>${content}</tbody></table>`);
+    res.end(`<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" /><meta charset="utf-8" /><table border="1"><tbody>${content}</tbody></table>`);
 }
 
 module.exports={respond};
