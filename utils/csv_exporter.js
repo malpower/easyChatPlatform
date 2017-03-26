3a3b32c05308ff9881887c535c26305cb9be0e77
Thrasky <Thrasky_Lou@Jabil.com>
2017-03-26 21:14:36 +0800

    malpower

--- utils/csv_exporter.js
+++ utils/csv_exporter.js
@@ -1,18 +1,19 @@
 function respond(list,title,header,res)
 {
-    if (header instanceof Array)
+    if (typeof(headers)==="string")
     {
-        header=header.join(",");
+        header=header.split(",");
     }
-    res.set("Content-Type","application/csv");
-    let content=`${title}\r\n`;
-    content+=header+"\r\n";
+    res.set("Content-Type","application/xls");
+    res.set("Content-DIsposition","attachment;filename=Export.xls");
+    let content=`<tr><td cols="${header.length}">${title}</td><tr>`;
+    content+=`<tr><td>${header.join("</td><td>")}</td></tr>`;
     for (let i=0;i<list.length;i++)
     {
-        list[i]=list[i].join(",");
+        list[i]="<td>"+list[i].join("</td><td>")+"</td>";
     }
-    content+=list.join("\r\n");
-    res.end(content);
+    content+="<tr>"+list.join("</tr><tr>")+"</tr>"
+    res.end(`<meta charset="utf-8" /><table><tbody>${content}</tbody></table>`);
 }
 
 module.exports={respond};
