e8e8f5148f4de07cb79aaabc69a1d4e89f56e337
Thrasky Louh <malpower@ymail.com>
2017-03-28 22:07:26 +0800

    table exportation enhanced.

--- utils/csv_exporter.js
+++ utils/csv_exporter.js
@@ -6,13 +6,13 @@ function respond(list,title,header,res)
     }
     res.set("Content-Type","application/xls");
     res.set("Content-Disposition","attachment;filename=Export.xls");
-    let content=`<tr><td cols="${header.length}">${title}</td><tr>`;
+    let content=`<tr><td colspan="${header.length}">${title}</td><tr>`;
     content+=`<tr><td>${header.join("</td><td>")}</td></tr>`;
     for (let i=0;i<list.length;i++)
     {
         list[i]="<td>"+list[i].join("</td><td>")+"</td>";
     }
-    content+="<tr>"+list.join("</tr><tr>")+"</tr>"
+    content+="<tr>"+list.join("</tr><tr>")+"</tr>";
     res.end(`<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" /><meta charset="utf-8" /><table border="1"><tbody>${content}</tbody></table>`);
 }
 
