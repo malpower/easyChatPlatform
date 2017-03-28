b665bd4716bfc02281462bc97136c912f3f51743
Maladrumthrasky <malpower@ymail.com>
2017-03-28 16:51:47 +0800

    message chaged

--- configurations/easyChat.js
+++ configurations/easyChat.js
@@ -1,4 +1,4 @@
 module.exports={appId: "e1c52c6f31364914a520f2b636308e2f",              //easy chat APP ID
                 appSec: "eeff237ba65e40baa1cef96c473ffd69",             //easy chat APP SEC
-                invitationText: "请使用浏览器登录 http://qdzy.internal-i-focusing.com/ 进入后台管理系统",         //invitation text for the subscribed users.
+                invitationText: "1、请使用浏览器登录 http://qdzy.internal-i-focusing.com/ \r\n2、用易信扫码功能扫码登录，进入后台管理系统。",         //invitation text for the subscribed users.
                 refreshFreq: 1000*60*30};                               //easy chat access token refresh frequence. better in one hour.
