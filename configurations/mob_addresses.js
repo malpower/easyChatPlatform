c024c2d4ee166d13ec6a280412fee0a185686ea1
Thrasky Louh <malpower@ymail.com>
2017-03-28 20:41:57 +0800

    bug fixed

--- configurations/mob_addresses.js
+++ configurations/mob_addresses.js
@@ -1,5 +1,5 @@
-let addr={"a1": "/mob/views/planningLayout.html?p=bujuguihua&p=xinxicankao",                //大事件
-          "a2": "/mob/views/planningLayout.html?p=bujuguihua&p=hangyejingxuan",                //行业精选
+let addr={"a1": "/mob/views/planningLayout.html?p=xinxicankao",                //大事件
+          "a2": "/mob/views/planningLayout.html?p=hangyejingxuan",                //行业精选
           "a3": "/mob/views/planningLayout.html?p=bujuguihua&",                //策划布局
           "a4": "/mob/views/planningLayout.html?p=mendiantuozhan&",                //门店拓展
           "a5": "/mob/views/planningLayout.html?p=xiaoshoucehua&",                    //销售策划
