113d98fe2950d2a2cc85998051edff6042b6a1ce
Maladrumthrasky <malpower@ymail.com>
2017-03-30 10:05:32 +0800

    AppID Changed.

--- configurations/easyChat.js
+++ configurations/easyChat.js
@@ -1,4 +1,4 @@
-module.exports={appId: "e1c52c6f31364914a520f2b636308e2f",              //easy chat APP ID
-                appSec: "eeff237ba65e40baa1cef96c473ffd69",             //easy chat APP SEC
+module.exports={appId: "98bf7dab0b964ccfa6d5f4c102cfdc2b",              //easy chat APP ID
+                appSec: "2229038bcd2142fab9e774838280662b",             //easy chat APP SEC
                 invitationText: "请使用浏览器访问 http://qdzy.internal-i-focusing.com\r\n扫描二维码登录管理后台。",         //invitation text for the subscribed users.
                 refreshFreq: 1000*60*30};                               //easy chat access token refresh frequence. better in one hour.
